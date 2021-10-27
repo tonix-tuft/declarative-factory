29c9621a2cc576c8a0218a6f83c1ef0c045fc80e
Anton Bagdatyev (Tonix) <antonytuft@gmail.com>
2021-10-27 23:24:50 +0200

    Removed spaces in doccomment

--- src/index.js
+++ src/index.js
@@ -48,12 +48,12 @@ const testConditionFn = testCondition =>
  *                                - factory: Anything. The value to return if "testCondition" is a truthy value
  *                                           or a function returning a truthy value.
  *
- *                            The last element of the returned tuples MAY not be a tuple array with two elements,
- *                            and in such case it will be treated as the default factory value to return if none
- *                            of the test conditions of the previous factories are satisfied.
- *                            If the last tuple is not a default value and none of the test conditions of the previous
- *                            factories are satisfied as well as for the factory of last tuple, then "undefined"
- *                            will be returned by this function.
+ *                          The last element of the returned tuples MAY not be a tuple array with two elements,
+ *                          and in such case it will be treated as the default factory value to return if none
+ *                          of the test conditions of the previous factories are satisfied.
+ *                          If the last tuple is not a default value and none of the test conditions of the previous
+ *                          factories are satisfied as well as for the factory of last tuple, then "undefined"
+ *                          will be returned by this function.
  * @param {Array|undefined} deps Dependencies array to use.
  * @return {*} The first factory value for which the test is truthy or returns a truthy value, a default factory value,
  *             or "undefined".
